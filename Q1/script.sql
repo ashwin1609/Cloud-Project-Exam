b3ee8020cf39c4c7e6a854d4e2e76a23a3b6ce30
Ashwin <ashwinsudharsan7@gmail.com>
2022-04-09 10:20:40 -0400

    push

---
+++ Q1/script.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS Covid19;
+USE Covid19;
+
+DROP TABLE IF EXISTS TimeSeries;
+
+
+CREATE TABLE IF NOT EXISTS TimeSeries (
+  id serial NOT NULL PRIMARY KEY,
+  Date DATE NOT NULL,
+  Country_Region varchar(200),
+  Province_State varchar(200),
+  Confirmed INTEGER,
+  Recovered INTEGER,
+  Deaths INTEGER
+
+  modified timestamp default CURRENT_TIMESTAMP NOT NULL,
+  INDEX `modified_index` (`modified`)
+);
+
+USE Covid19;
+LOAD DATA INFILE '/home/ashwinsudharsan7/Cloud-Project-Exam/Q1/time-series-19-covid-combined.csv'
+INTO TABLE table_name
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '/n'
+IGNORE 1 ROWS;
+
+
+
+
